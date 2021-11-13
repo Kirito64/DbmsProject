c646e41ddb7aadb1607f4844e211c596472e2526
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-11-13 12:28:53 +0530

    minor db fix

--- backend/models/studentPayment.js
+++ backend/models/studentPayment.js
@@ -2,7 +2,6 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../configs/sequelize");
 
 const StudentPayment = sequelize.define("studentPayment", {
-  JEER: { type: DataTypes.STRING },
   JoDD: { type: DataTypes.STRING },
   InDD: { type: DataTypes.STRING },
 });
